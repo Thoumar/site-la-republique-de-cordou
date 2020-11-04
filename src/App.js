f8289728c4058ade73bcc0539958366d218eeada
Thoumar <thomas@thoumar.com>
2020-11-04 14:50:53 +0100

    Small CSS modifications for better use of space and mre fidelity to the mockups

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
       <div className="App__Text">
         <h1 className="App__title">La république de Cordou</h1>
         <p className="App__description">
-          Xavier Fourtou, psychanalyste et auteur de ce conte illustré
-          <br />
-          avec talent par la designer Lisa Chaput, nous livre une histoire
-          <br />
-          tendre et sociale qui donne envie de vivre plus fort ici et ailleurs.
+          Xavier Fourtou, psychanalyste et auteur<br />
+          de ce conte illustré avec talent par la<br />
+          designer Lisa Chaput, nous livre une<br />
+          histoire tendre et sociale qui donne envie<br />
+          de vivre plus fort ici et ailleurs.
         </p>
         <img className="App__Icon" src={soundIcon} alt='Sound icon' />
       </div>
